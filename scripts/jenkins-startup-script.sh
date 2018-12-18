ee1924d28d7d55db36a7249ec708416d4afc9fab
Hamdy Elbatal <elbatalhamdy77@gmail.com>
2018-12-18 10:43:24 +0000

    Update jenkins-startup-script.sh

--- scripts/jenkins-startup-script.sh
+++ scripts/jenkins-startup-script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Install jenkins
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
