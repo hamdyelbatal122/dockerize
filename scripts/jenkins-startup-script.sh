42166e42acbc031121e8b2ad66875c40d8d0f336
Hamdy Elbatal <elbatalhamdy77@gmail.com>
2020-12-04 10:28:24 +0200

    Update jenkins-startup-script.sh

--- scripts/jenkins-startup-script.sh
+++ scripts/jenkins-startup-script.sh
@@ -4,3 +4,9 @@ wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
+
+
+# Add jenkins to the docker group
+sudo gpasswd -a jenkins docker
+sudo service jenkins restart
+
